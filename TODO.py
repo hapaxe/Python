ad1d943b517ad0922b0066ac80841226e6b64e3d
Martin L'Anton <lantonmartin@gmail.com>
2017-08-23 10:55:24 -0700

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -15,18 +15,22 @@ MAYA_standard_publish_template_file_name = '[depth4]_[depth5]_[depth6]_PUBLISH.e
 MAYA_folder_dependent_publish_template_file_name = {'ASSETS': '[depth4]_[depth5].ext',
                                                     'ANIMATION': '[depth5]_[depth6].ext'}
 
-depth = 6
-hierarchy = {'depth0': [PROJECT_PATH_1, PROJECT_PATH_2],
-             'depth1': {PROJECT_PATH_1: [**PROJECTS],
-                        PROJECT_PATH_2: [**PROJECTS1]},
-             'depth2': ['scenes', 'sounds', 'sourceimage'],
-             'depth3': ['ASSETS', 'ANIMATION'],
-             'depth4': {'ASSETS': ['CH', 'PR', 'VEH'],
-                        'ANIMATION': {**episodes}},
-             'depth5': {'ASSETS': {**assets},
-                        'ANIMATION': {**shots}},
-             'depth6': {'ASSETS': ['MODEL', 'RIG', 'TEXTURE'],
-                        'ANIMATION': ['LAYOUT', 'ANIM']}}
+hierarchy_template_1_depth = 6
+hierarchy_template_1 = {'depth0': [PROJECT_PATH_1, PROJECT_PATH_2],
+                        'depth1': [**PROJECTS],
+                        'depth2': ['scenes', 'sounds', 'sourceimage'],
+                        'depth3': ['ASSETS', 'ANIMATION'],
+                        'depth4': {'ASSETS': ['CH', 'PR', 'VEH'],
+                                   'ANIMATION': {**episodes}},
+                        'depth5': {'ASSETS': {**assets},
+                                   'ANIMATION': {**shots}},
+                        'depth6': {'ASSETS': ['MODEL', 'RIG', 'TEXTURE'],
+                                   'ANIMATION': ['LAYOUT', 'ANIM']}}
+
+hierarchy_template_2_depth = 2
+hierarchy_template_2 = {'depth0': PROJECT_PATH_2,
+                        'depth1': [**ASSETS],
+                        'depth2': ['Animation', 'Rig', 'Model']}
 
 # 4 ways to create folders : fixed, variable, variable by folder, fixed by
 # folder
@@ -34,9 +38,9 @@ hierarchy = {'depth0': [PROJECT_PATH_1, PROJECT_PATH_2],
 
 # Create .json file storing project relative folders :
 
+# EXAMPLE HIERARCHY TEMPLATE 1 --------------------
 # Stored at depth1
 PROJECTS = ['project1', 'project2', 'project3', 'project4']
-PROJECTS1 = ['project1', 'project2', 'project3']
 
 # Stored at depth4
 episodes = ['episode1', 'episode2', 'episode3']
@@ -48,5 +52,9 @@ assets = {'CH': ['Anna', 'Ziggs'],
 shots = {'episode1': ['shot1', 'shot2'],
          'episode2': ['shot1', 'shot2', 'shot3'],
          'episode3': ['shot1']}
+
+# EXAMPLE HIERARCHY TEMPLATE 2 --------------------
+# Stored at depth1
+ASSETS = ['Asset1', 'Asset2', 'Asset3']
          
 # Create .json file storing user preferences : number of last paths stored as well as last paths stored
