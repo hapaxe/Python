a8e18e9e0185fbb30cb5971e628dab9a7b59b35f
Martin L'Anton <lantonmartin@gmail.com>
2017-08-23 10:32:52 -0700

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -1,8 +1,8 @@
  # Create .json file storing the MAYA_PROJECT_PATH and MAX_PROJECT_PATH as well as the depth and folder hierarchy for both MAYA and MAX :
 # Save number of digits for increments and file name template in the file too 
 
-MAYA_PROJECT_PATH = 'D:\Project\NeoPlum\Art\Characters'
-MAX_PROJECT_PATH = 'D:\Project\NeoPlum\Art\Levels\Hotel\Art'
+PROJECT_PATH_1 = 'D:\Project\NeoPlum\Art\Characters'
+PROJECT_PATH_2 = 'D:\Project\NeoPlum\Art\Levels\Hotel\Art'
 
 increment_digits = 4
 
@@ -15,17 +15,18 @@ MAYA_standard_publish_template_file_name = '[depth4]_[depth5]_[depth6]_PUBLISH.e
 MAYA_folder_dependent_publish_template_file_name = {'ASSETS': '[depth4]_[depth5].ext',
                                                     'ANIMATION': '[depth5]_[depth6].ext'}
 
-MAYA_depth = 6
-MAYA_hierarchy = {'depth0': [MAYA_PROJECT_PATH, MAX_PROJECT_PATH]
-                  'depth1': ['PROJECT1', 'PROJECT2', 'PROJECT3'],
-                  'depth2': ['scenes', 'sounds', 'sourceimage'],
-                  'depth3': ['ASSETS', 'ANIMATION'],
-                  'depth4': {'ASSETS': ['CH', 'PR', 'VEH'],
-                             'ANIMATION': [episodes]},
-                  'depth5': {'ASSETS': {assets},
-                             'ANIMATION': {shots}},
-                  'depth6': {'ASSETS': ['MODEL', 'RIG', 'TEXTURE'],
-                             'ANIMATION': ['LAYOUT', 'ANIM']}}
+depth = 6
+hierarchy = {'depth0': [PROJECT_PATH_1, PROJECT_PATH_2],
+             'depth1': {PROJECT_PATH_1: [**PROJECTS],
+                        PROJECT_PATH_2: [**PROJECTS1]},
+             'depth2': ['scenes', 'sounds', 'sourceimage'],
+             'depth3': ['ASSETS', 'ANIMATION'],
+             'depth4': {'ASSETS': ['CH', 'PR', 'VEH'],
+                        'ANIMATION': {**episodes}},
+             'depth5': {'ASSETS': {**assets},
+                        'ANIMATION': {**shots}},
+             'depth6': {'ASSETS': ['MODEL', 'RIG', 'TEXTURE'],
+                        'ANIMATION': ['LAYOUT', 'ANIM']}}
 
 # 4 ways to create folders : fixed, variable, variable by folder, fixed by
 # folder
@@ -33,8 +34,14 @@ MAYA_hierarchy = {'depth0': [MAYA_PROJECT_PATH, MAX_PROJECT_PATH]
 
 # Create .json file storing project relative folders :
 
+# Stored at depth1
+PROJECTS = ['project1', 'project2', 'project3', 'project4']
+PROJECTS1 = ['project1', 'project2', 'project3']
 
+# Stored at depth4
 episodes = ['episode1', 'episode2', 'episode3']
+
+# Stored at depth 5
 assets = {'CH': ['Anna', 'Ziggs'],
           'PR': ['Chair', 'Table', 'Bowl', 'Spoon'],
           'VEH': ['Bike', 'Car']}
